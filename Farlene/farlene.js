2cdc01f852e0fad7235e64032a682cedcc78b5de
flanflanagan <rrf4@buffalo.edu>
2017-10-12 00:19:02 -0400

    adding popup marks

--- Farlene/farlene.js
+++ Farlene/farlene.js
@@ -1,28 +1,3 @@
 // Farlene Functions and variables//
-var mapBool = false;
 
-function worldMap(){
-	if(mapBool == true){
-		node = document.getElementById('farlene');
-		document.getElementById('vizFrame').removeChild(node);
-		mapBool = false;
-		map = null;
-		return;
-	}
-	if(mapBool == false){
-		var div = document.createElement("div");
-		div.id = 'farlene';
-		$("#vizFrame").append(div);
-		
-		var map = L.map('farlene', {
-			crs: L.CRS.Simple
-		});
-	
-		var bounds = [[0,0], [1000,1500]];
-		var image = L.imageOverlay('Farlene/images/farlene.png', bounds).addTo(map);
-		map.fitBounds(bounds);
-		mapBool = true;	
-		return;	
-	}
-}
 
--- Farlene/mappoints.js
+++ Farlene/mappoints.js
@@ -1,10 +1,35 @@
 //Sets up the map points//
 
+var mapBool = false;
 
-var map = L.map('farlene', {
-    crs: L.CRS.Simple
-});
+function worldMap(){
+	if(mapBool == true){
+		node = document.getElementById('farlene');
+		document.getElementById('vizFrame').removeChild(node);
+		mapBool = false;
+		map = null;
+		return;
+	}
+	if(mapBool == false){
+		var div = document.createElement("div");
+		div.id = 'farlene';
+		$("#vizFrame").append(div);
+		
+		var map = L.map('farlene', {
+			crs: L.CRS.Simple
+		});
+		map = addPoints(map);
+		var bounds = [[0,0], [1000,1500]];
+		var image = L.imageOverlay('Farlene/images/farlene.png', bounds).addTo(map);
+		map.fitBounds(bounds);
+		mapBool = true;	
+		return;	
+	}
+}
 
-var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay('images/farlene.png', bounds).addTo(map);
-map.fitBounds(bounds);
+function addPoints(map){
+	var sol = L.latLng([ 500, 980]);
+	L.marker(sol).addTo(map).bindPopup('Montess City');
+	return map;
+}
+
