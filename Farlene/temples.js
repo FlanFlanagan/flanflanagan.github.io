521ffeccc7780f50be25d49d1624fa7319b51bb8
flanflanagan <rrf4@buffalo.edu>
2017-10-14 11:59:33 -0400

    added temples.js

---
+++ Farlene/temples.js
@@ -0,0 +1,36 @@
+var temples = {
+	"Desiccator": {
+		"y": 552.3,
+		"x": 895.1,
+		"info": 'This temple houses the desiccator. The desiccator is a plain rusty dagger. Its physical properties are like that of the other Nithlum weapons; light, strong, and deadly.' +
+		'The temple also houses the stopplebanger.',
+		"notes": ['While Mark was retriveing the weapon, the spirit room began to fill with water. The problem was solved by filling the bag of holding with as much water as it could possible hold',
+				  'This temple also had several traps inside of it that appeared to have been previously triggered, though it is not known by what.',
+				  'The stopplebanger escape or was released before Ziggy and Co. arrived.'], 
+	},
+	"Harmony": {
+		"y": 543,
+		"x": 979.1,
+		"info": 'This temple is located in a tunnel system seemingly built into a short cliff side. The tunnel seems to be a combination of a gnoll encampment and the Nithlum temple. The spirit room in this temple is located '+
+		'near the back of the tunnel system. The room is decorated as per Harmony\'s wishes. Musical elements and festivities are painted into the rooms moral.',
+		"notes": [], 
+	},
+	"Temple Name": {
+		"y": 448,
+		"x": 921,
+		"info": "This temple is dedicated to the axe Furance. It is a small temple, the largest room is the spirit room. The paintings on the wall depict a series of advanced and powerful flow movements while using" + 
+		' an axe in combat. Each painting is borded by a dark green flame',
+		"notes": [], 
+	}
+};
+
+
+
+/*
+	"Temple Name": {
+		"y": ,
+		"x": ,
+		"info": "",
+		"notes": [], 
+	},
+*/
