6f28899932f5d8b8c656138cf5883536f92f4905
flanflanagan <rrf4@buffalo.edu>
2017-10-13 22:16:54 -0400

    awww yeahhh

--- Farlene/cities.js
+++ Farlene/cities.js
@@ -1,12 +1,56 @@
 var cities = {
 	"Montess City": {
-		"x": "502.0",
-		"y": "979.0",
-		"info": 'Montess City is the capital of Montess. It is the largest city in the Fiefdom. It also serves as the largest producer of fine lumber. The best craftsmen in the fiefdom can be found in this city.'
+		"y": 502.3,
+		"x": 979.1,
+		"pop": "Large",
+		"info": 'Montess City is the capital of Montess. It is the largest city in the Fiefdom. It also serves as the largest producer of fine lumber. The best craftsmen in the fiefdom can be found in this city.',
+		"stores": [],
+		"notes": [], 
 	},
 	"Winstmarch City": {
-		"x": "441.0",
-		"y": "929.0",
-		"info": 'Montess City is the capital of Montess. It is the largest city in the Fiefdom. It also serves as the largest producer of fine lumber. The best craftsmen in the fiefdom can be found in this city.'
-	}
+		"y": 441.2,
+		"x": 929.1,
+		"pop": "Middle-sized",
+		"info": 'Winstmarch City is a city that survives on its tourism. The major commodity is the, exceedingly rare and very difficult to hunt, Dresdres Cacoo. The feathers of the Dresdres Cacoo shimmer through' +
+		'the colors of the visible spectrum while it is alive. A dead cacoo still maintains a beautiful sheen on it\'s plumage, however the colour pattern is locked into the color pattern it was displaying upon it\'s death.' ,
+		"stores": [],
+		"notes": [], 
+	},
+	"Forstall": {
+		"y": 386,
+		"x": 954,
+		"pop": "Small",
+		"info": "Primarily a logging and farming town. There are two lumber camps here that perform sustainable logging - monitored by the Montess Fiefdom leadership.",
+		"stores": [],
+		"notes": [], 
+	},
+	"Yuhlin City": {
+		"y": 342,
+		"x": 963,
+		"pop": "Medium-sized",
+		"info": "Captial of Farmlia. \nLarge shipping port. Most of the lumber and other trade goods from the eastern portion of Farlene flow through here.",
+		"stores": [],
+		"notes": [], 
+	},
+	"Rockfish": {
+		"y": 536,
+		"x": 904,
+		"pop": "Very small-sized",
+		"info": "A small mining town. The majority of the population works in the black salt mines. Black salt is the primary export of the city.",
+		"stores": [],
+		"notes": ["Stopplebanger was discovered here, along with Dessicator", "Bilberry turned on the group and killed Mark and gravely wounded Barston"], 
+	},
+};
+
+
+
+/*
+"Forstall": {
+	"y": ,
+	"x": ,
+	"pop": "",
+	"info": "",
+	"stores": [],
+	"notes": [], 
 }
+*/
--- Farlene/mappoints.js
+++ Farlene/mappoints.js
@@ -37,19 +37,72 @@ function worldMap(){
 function addPoints(map){
 	for(var key in cities){
 		var city = cities[key];
-		y = parseFloat(city['y']); x = parseFloat(city['x']);
-		var circle = L.circle([y,x], {
-	    	color: 'black',
-	    	radius: 6,
-	    	weight: 1,
-	   		fillOpacity: 0
-		}).addTo(map);
+		y = parseFloat(city.y); x = parseFloat(city.x);
+		var circle = L.marker([y,x], {
+	   		bubblingMouseEvents: false
+		});
+		circle.city = city;
 		circle.on('click', function(e){
-			$('#farleneInfo').empty();
-    		$('#farleneInfo').append(city['info']);
+			backButton(e.target.city);
 		});
 		circle.addTo(map);
 	}
 	return map;
+}
+
+function openInfo(city){
+	$('#farleneInfo').empty();
+	$('#farleneInfo').append('Population: ' + city.pop);
+	p = document.createElement('p');
+	p.append(city.info);
+	$('#farleneInfo').append(p);
+	$('#farleneInfo').append(genBackButt(city));
+}
+
+function openStores(city){
+	$('#farleneInfo').empty();
+	$('#farleneInfo').append(genBackButt(city));
+}
+
+function openNotes(city){
+	$('#farleneInfo').empty();
+	for(var i = 0; i < city.notes.length; i++){
+		p = document.createElement('p');
+		p.append(city.notes[i]);
+		$('#farleneInfo').append(p);	
+	}
+	$('#farleneInfo').append(genBackButt(city));
+}
+
+function backButton(city){
+	$('#farleneInfo').empty();
+	info = genButton('Information');
+	info.onclick = function(){
+		openInfo(city);
+	};
+	stores = genButton('Stores');
+	stores.onclick = function(){
+		openStores(city);
+	};
+	notes = genButton('Notes');
+	notes.onclick = function(){
+		openNotes(city);
+	};
+	var eleArray = [info, stores, notes];
+	$('#farleneInfo').append(eleArray);
+}
+
+function genButton(name){
+	button = document.createElement('button');
+	button.textContent = name;
+	return button;
+}
+
+function genBackButt(city){
+	back = genButton("Back");
+	back.onclick = function(){
+		backButton(city);
+	};	
+	return back;
 }
 
