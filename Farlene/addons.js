28372497cf2028d93a6dcbe5cbd5612ee53e0a11
flanflanagan <rrf4@buffalo.edu>
2018-01-08 11:24:20 -0500

    forgot to add

---
+++ Farlene/addons.js
@@ -0,0 +1,7 @@
+
+var templeIcon = L.icon({
+    iconUrl: 'Farlene/images/temple.png',
+
+    iconSize:     [25, 25], // size of the icon
+    //iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+});
