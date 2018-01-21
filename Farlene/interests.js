ebf2281778b9f42d5e8bd0e48a57ab767a5db09f
flanflanagan <rrf4@buffalo.edu>
2018-01-20 21:53:10 -0500

    more nest info

--- Farlene/interests.js
+++ Farlene/interests.js
@@ -1,8 +1,8 @@
 var interests = {
-	"CacooNest": {
+	"Dresdres Cacoo Nest": {
 		"y": 415.3,
 		"x": 920.1,
-		"info": 'Dresdres Cacoo Nest',
+		"info": 'Ziggy found a Dresdres Cacoo nest in this location. A cacoo sat on the nest the whole time so he did not get a chance to see if she was nesting eggs.',
 		}
 };
 
