2dfced61f2b0733ce51c4e9e1e5d0594f8de7d00
FlanFlanagan <rf8465@utexas.edu>
2019-12-01 10:21:42 -0700

    added new data structure

---
+++ Farlene/data.js
@@ -0,0 +1,121 @@
+var data = {
+    'farlene':{
+        'temples' : {
+            "Desiccator": {
+                "y": 552.3,
+                "x": 895.1,
+                "info": 'This temple houses the desiccator. The desiccator is a plain rusty dagger. Its physical properties are like that of the other Nithlum weapons; light, strong, and deadly.' +
+                'The temple also houses the stopplebanger.',
+                "notes": ['While Mark was retriveing the weapon, the spirit room began to fill with water. The problem was solved by filling the bag of holding with as much water as it could possible hold',
+                        'This temple also had several traps inside of it that appeared to have been previously triggered, though it is not known by what.',
+                        'The stopplebanger escape or was released before Ziggy and Co. arrived.'], 
+            },
+            "Harmony": {
+                "y": 543,
+                "x": 979.1,
+                "info": 'This temple is located in a tunnel system built into a short cliff side. The temple entrance appears to have been taken over by a gnoll encampment. The spirit room in this temple is located '+
+                'near the back of the tunnel system. The room is decorated with musical elements and festivities painted into murals on the walls.',
+                "notes": [], 
+            },
+            "Furance": {
+                "y": 448,
+                "x": 921,
+                "info": "This temple is dedicated to the axe Furance. It is a small temple, the largest room is the spirit room. The paintings on the wall depict a series of advanced and powerful flow movements while using" + 
+                ' an axe in combat. Each painting is bordered by a dark green flame',
+                "notes": [], 
+            }
+        },
+        'cities': {
+            "Montess City": {
+                "y": 502.3,
+                "x": 979.1,
+                "pop": "Large",
+                "info": 'Montess City is the capital of Montess. It is the largest city in the Fiefdom. It also serves as the largest producer of fine lumber. The best craftsmen in the fiefdom can be found in this city.',
+                "stores": [],
+                "notes": [], 
+            },
+            "Winstmarch City": {
+                "y": 441.2,
+                "x": 929.1,
+                "pop": "Medium",
+                "info": "Winstmarch City is a city that survives on its tourism. The major commodity is the, exceedingly rare and very difficult to hunt, Dresdres Cacoo. The feathers of the Dresdres Cacoo shimmer through" +
+                "the colors of the visible spectrum while it is alive. A dead cacoo still maintains a beautiful sheen on its plumage, however the colour pattern is locked into the color pattern it was displaying upon its death." ,
+                "stores": [],
+                "notes": [], 
+            },
+            "Forstall": {
+                "y": 386,
+                "x": 954,
+                "pop": "Small",
+                "info": "Primarily a logging and farming town. There are two lumber camps here that perform sustainable logging - monitored by the Montess Fiefdom leadership.",
+                "stores": [],
+                "notes": [], 
+            },
+            "Yuhlin City": {
+                "y": 342,
+                "x": 963,
+                "pop": "Medium",
+                "info": "Captial of Farmlia. \nLarge shipping port. Most of the lumber and other trade goods from the eastern portion of Farlene flow through here.",
+                "stores": [],
+                "notes": [], 
+            },
+            "Rockfish": {
+                "y": 536,
+                "x": 904,
+                "pop": "Very small",
+                "info": "A small mining town. The majority of the population works in the black salt mines. Black salt is the primary export of the city.",
+                "stores": [],
+                "notes": ["Stopplebanger was discovered here, along with Desiccator", "Bilberry turned on the group here and killed Mark. He also gravely wounded Barston"], 
+            },
+            "Dawns Light": {
+                "y": 358,
+                "x": 787,
+                "pop": "Very Large",
+                "info": "A very large town. This is also the wealthiest town in Pastellia. The council and the king meet here to discuss political matters. " +
+                        "The highest concentration of skilled craftsmen and artistics live here. It also rivals Yuhlin as one of the largest shipping ports in Farlene." +
+                        "What little standing military Pastellia has is based out of this city.",
+                "stores": [],
+                "notes": ["Capital of Pastellia",], 
+            }
+        },
+        'interests': {
+            "Dresdres Cacoo Nest": {
+                "y": 415.3,
+                "x": 920.1,
+                "info": 'Ziggy found a Dresdres Cacoo nest in this location. A cacoo sat on the nest the whole time so he did not get a chance to see if she was nesting eggs.',
+                }
+            
+        }
+    },
+    'hamlet':{
+        'temples' : {
+            "Desiccator": {
+                "y": 552.3,
+                "x": 895.1,
+                "info": 'This temple houses the desiccator. The desiccator is a plain rusty dagger. Its physical properties are like that of the other Nithlum weapons; light, strong, and deadly.' +
+                'The temple also houses the stopplebanger.',
+                "notes": ['While Mark was retriveing the weapon, the spirit room began to fill with water. The problem was solved by filling the bag of holding with as much water as it could possible hold',
+                        'This temple also had several traps inside of it that appeared to have been previously triggered, though it is not known by what.',
+                        'The stopplebanger escape or was released before Ziggy and Co. arrived.'], 
+            },
+        },
+        'cities': {
+            "Montess City": {
+                "y": 502.3,
+                "x": 979.1,
+                "pop": "Large",
+                "info": 'Montess City is the capital of Montess. It is the largest city in the Fiefdom. It also serves as the largest producer of fine lumber. The best craftsmen in the fiefdom can be found in this city.',
+                "stores": [],
+                "notes": [], 
+            },
+        },
+        'interests': {
+            "Dresdres Cacoo Nest": {
+                "y": 415.3,
+                "x": 920.1,
+                "info": 'Ziggy found a Dresdres Cacoo nest in this location. A cacoo sat on the nest the whole time so he did not get a chance to see if she was nesting eggs.',
+            }
+            
+        }
+    }
+}
