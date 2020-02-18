243af7167470280b4f84b366973022defe6a6726
FlanFlanagan <rf8465@utexas.edu>
2020-02-18 14:09:57 -0700

    Update stores.js

--- Farlene/stores.js
+++ Farlene/stores.js
@@ -8,7 +8,8 @@ var items = {
         "from afar. The bow has 3 charges. When you fire an arrow from this bow, you can speak the command word and use a charge to " +
         "see through the arrow as a bonus action. You can see from the point your arrow lands as if you were there for 1 minute. " +
         "If the arrow strikes a creature, you see what they see for the same duration. The bow regains 1d3 charges each day at dawn.",
-        "Cost": "2000 gold"
+        "Cost": "2000 gold",
+        "Quantity": 1
     },
     "Nest Charm": {
         "type": "item",
@@ -18,40 +19,46 @@ var items = {
         " 30-foot-diameter circular platform of interwoven branches. Once the platform has reached its full size, a magical tree grows beneath it, lifting the platform 50 feet into the air, "+
         "carrying any passengers and items along with it. This perch lasts up to 8 hours, at which point the tree will retreat into the ground and the platform will shrink, eventually " +
         "curling back into the small charm. At any time the command word can be spoken again to dismiss the perch in a similar fashion. Once the nest charm’s power",
-        "Cost": "10000 gold"
+        "Cost": "10000 gold",
+        "Quantity": 1
     },
     "Potion of Bravery": {
         "type": "potion",
         "Classification":"Uncommon Item",
         "Description": "This potion will remove any fear affects currently affecting an individual. Additionally it will provide 24 hours of fear immunity.",
-        "Cost": "100 gold"
+        "Cost": "100 gold",
+        "Quantity": 1
     },
     "Potion of Invigoration": {
         "type": "potion",
         "Classification":"Uncommon Item",
         "Description": "This potion will remove one point of exhaustion from the imbiber. Created by Tessa Smoak at Hamlet branch of the Alchemical Society.",
-        "Cost": "50 gold"
+        "Cost": "50 gold",
+        "Quantity" : 0
     },
     "Potion of Healing": {
         "type": "potion",
         "Classification":"Uncommon Item",
         "Description": "Restores 2d4+2 hit points to the imbiber.",
-        "Cost": "50 gold"
+        "Cost": "50 gold",
+        "Quantity": 1;
     },
     "Potion of Greater Healing": {
         "type": "potion",
         "Classification":"Uncommon Item",
         "Description": "Restores 4d4+4 hit points to the imbiber.",
-        "Cost": "100 gold"
+        "Cost": "100 gold",
+        "Quantity" : 0
     },
 }
 
 /* This is a layout for the items
 "": {
     "type": "",
-    "itemType": "",
-    "stats": "",
-    "description": "",
-    "cost": ""
+    "ItemType": "",
+    "Stats": "",
+    "Description": "",
+    "Cost": "",
+    "Quantity":
 },
 */
